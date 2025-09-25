3eafe6b748f300ae62fb6d3e05816d15708841b1
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-26 01:20:04 +0530

    Variables session

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 #####Date Variables#####
 
-DATE=$(date)
-echo "Today's date is: $DATE"
+START_TIME=$(date +%s) # Capture start time in seconds since
+
+sleep 10& # Simulate a process with sleep
+
+END_TIME=$(date +%s) # Capture end time in seconds since
+
+TOTAL_TIME=$((END_TIME - START_TIME)) # Calculate total time taken
+
+echo "Script execution time: $TOTAL_TIME seconds"
