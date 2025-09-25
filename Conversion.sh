247b3a0ed23d1bed42d2150e74b87059d874727c
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-25 19:18:04 +0530

    committing conversion

--- Conversion.sh
+++ Conversion.sh
@@ -3,4 +3,4 @@
 echo "Mahesh::Hi Suresh, How are you?"
 echo "Suresh::I am fine, How are you?"
 echo "What you learnt recently?"
-echo "Am learning shell scripting:
+echo "Am learning shell scripting:"
