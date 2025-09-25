dd3d6e7d02445d9ddd3be4b61aed2299d1d0c970
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-25 19:38:11 +0530

    some message

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PERSON1=Naresh
-PERSON2=Ramesh
+PERSON1=Harish
+PERSON2=Shankar
 echo "$PERSON1::Hi $PERSON2, How are you?"
 echo "$PERSON2::I am fine $PERSON1, How are you?"
 echo "$PERSON1::What you learnt recently $PERSON2?"
-echo "$PERSON2::Am learning shell scripting $PERSON2:"
+echo "$PERSON2::Am learning shell scripting $PERSON1:"
