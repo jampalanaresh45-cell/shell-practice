d2a0ef2aa0209cdf7f5a16360f42dbdbfec002cd
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-25 20:15:17 +0530

    Variables session

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo  "$PERSON1:: Welcome to Shell Scripting, $PERSON2"
+echo  "$PERSON2:: Thank you $PERSON1, Good to be here"
+echo  "$PERSON1:: What do you want to learn today?"
+echo  "$PERSON2:: I want to learn about Variables in Shell Scripting"
