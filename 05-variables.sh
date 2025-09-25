d21cdab93a4fc77b116ee57a27aaeac902bbdc3e
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-25 23:25:40 +0530

    Variables session

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 ####Password as argument and use it in the script####
 
 echo "Enter your password:"
-read Password # Read password from user input
+read -s Password # Read password from user input
 
 echo "Password entered is: $Password"
