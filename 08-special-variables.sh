500f56691ce4f4b67c1e2ee2d68f592e826ea4c4
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-26 14:04:15 +0530

    Variables session

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -4,9 +4,9 @@
 echo "special variables: $@" ##To accept all
 echo "special variables: $*"
 echo "script variable $0" ##To accept script name
-echo "special char variable for current Directory $PWD"
-echo "special char variable for current user $USER"
-echo "special char variable for current home directory $HOME"
+echo "special char variable for current Directory: $PWD"
+echo "special char variable for current user: $USER"
+echo "special char variable for current home directory: $HOME"
 echo "special PID exit status: $$"
 sleep 10 &
-echo "special char variable for last background process id $!"
+echo "special char variable for last background process id: $!"
