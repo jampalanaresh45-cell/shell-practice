1a67c471d0a47314dd3519884a9e8940cc0c1bc6
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-27 14:38:41 +0530

    Variables session

--- datatypes.sh
+++ datatypes.sh
@@ -8,4 +8,4 @@ SUM=$((NUMBER1 + NUMBER2))
 echo "SUM is ${SUM}"
 
 Contents=("AWS" "Linux" "Shell")
-echo "the present contents of the Devsecops course are: ${contents[@]}"
+echo "the present contents of the Devsecops course are:" ${Contents[@]}"
