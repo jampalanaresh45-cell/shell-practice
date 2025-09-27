717e74cdc2efbc20b184525238a2e439bebeb868
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-28 00:10:45 +0530

    Variables session

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,10 +4,12 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: User must have privilege accesss"
+    exit 1
     fi
     dnf install mysql -y
     if [ $? -ne 0 ]; then
         echo "ERROR::MYSQL installation failed"
+        exit 1
     else
         echo "MySQL installation succeeded"
     fi
