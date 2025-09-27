7518f303a0d6ab3ce594c2d4477eaac79cc78b4d
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-27 21:09:44 +0530

    Variables session

--- 10-Evencondition.sh
+++ 10-Evencondition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###Even number check in shell script###
 read NUMBER
-if [(( $NUMBER % 2 )) -eq 0 ]; then
+if [ (($NUMBER % 2)) -eq 0 ]; then
     echo "The given $NUMBER is even"
 else
     echo "The given $NUMBER is odd"
