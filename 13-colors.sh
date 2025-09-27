ca7f7228755215b4117bffdbe1c622448f2dd5c9
jampalanaresh45-cell <jampalanaresh45@gmail.com>
2025-09-28 01:07:52 +0530

    Variables session

---
+++ 13-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+####Color codes in shell script####
+R="\e[31m" #Red
+G="\e[32m" #Green
+Y="\e[33m" #Yellow
+N="\e[0m"  #No Color
+
+echo -e "$G This is Green Color"
+echo "check the color"
