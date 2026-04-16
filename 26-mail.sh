bb33ae17cba4d5cc49814beb33328c50fe31a983
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-17 00:36:20 +0530

    shell-pracice

--- 26-mail.sh
+++ 26-mail.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-
 TO_ADDRESS="$1"
 SUBJECT="$2"
 ALERT_TYPE="$3"
 MESSAGE_BODY="$4"
-IP_ADDRESS="$5"
-To_TEAM="$6"
+FORMATTED_BODY=$(printf '%s \n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
+IP_ADDRESS=$5
+To_TEAM=$6
 
-escape() {
-  printf '%s' "$1" | sed 's/[&|]/\\&/g'
-}
-
-FORMATTED_BODY=$(escape "$MESSAGE_BODY")
-To_TEAM=$(escape "$To_TEAM")
-ALERT_TYPE=$(escape "$ALERT_TYPE")
-IP_ADDRESS=$(escape "$IP_ADDRESS")
-
-FINAL_BODY=$(sed -e "s|TO_TEAM|$To_TEAM|g" \
-                 -e "s|ALERT_TYPE|$ALERT_TYPE|g" \
-                 -e "s|IP_ADDRESS|$IP_ADDRESS|g" \
-                 -e "s|MESSAGE|$FORMATTED_BODY|g" template.html)
-
+FINAL_BODY=$(sed -e "s/TO_TEAM/$To_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
