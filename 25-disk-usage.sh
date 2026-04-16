8f49331e057c055dea166202b88ac68e973fc559
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-16 22:49:23 +0530

    shell-roboshop-common

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=2 ## In project we keepp it as 75 ##
+DISK_THRESHOLD=75 ## In project we keepp it as 75 ##
 IPADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 MESSAGE=""
 
