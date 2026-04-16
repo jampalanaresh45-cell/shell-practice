6d983e43d8f7818edfc22814010e41861eab23ac
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-16 22:47:13 +0530

    shell-roboshop-common

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 ## In project we keepp it as 75 ##
+IPADDRESS=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
+
 while IFS= read -r line; 
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -gt $DISK_THRESHOLD ]; then
-        echo "Disk usage for partition $PARTITION is above threshold: $USAGE%"
+        MESSAGE+="Disk usage on partition $PARTITION is above threshold: $USAGE% \n"
     fi
 done <<< "$DISK_USAGE"
+
+echo Message Body :$MESSAGE
