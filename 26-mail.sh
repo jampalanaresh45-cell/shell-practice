e2ad049e0792d351fddb0cb772da2e335ed8d166
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-17 00:20:34 +0530

    shell-pracice

--- 26-mail.sh
+++ 26-mail.sh
@@ -2,11 +2,12 @@
 TO_ADDRESS="$1"
 SUBJECT="$2"
 ALERT_TYPE="$3"
-MESSAGE_BODY="$(echo $4 | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")"
+MESSAGE_BODY="$4"
+FORMATTED_BODY=$(print '%s \n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
 IP_ADDRESS=$5
 To_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$To_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$To_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
 {
 echo "To: $TO_ADDRESS"
 echo "Subject: $SUBJECT"
