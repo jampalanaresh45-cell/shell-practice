0e966dc46cf4c4f4696054a2b29d6215843355b3
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-16 15:43:22 +0530

    shell-roboshop-common

--- 23-while-loop.sh
+++ 23-while-loop.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-count=5
+# count=5
 
-echo "staring countdown ..."
-while [ $count -gt 0 ]
- do
-    echo "Time left: $count"
-    sleep 1
-    count=$((count-1))
-done
+# echo "staring countdown ..."
+# while [ $count -gt 0 ]
+#  do
+#     echo "Time left: $count"
+#     sleep 1
+#     count=$((count-1))
+# done
 
-echo "Time's up!"
+# echo "Time's up!"
+
+#!/bin/bash
+file="20-script.sh"
+
+while IFS= read -r line; do
+    echo "Processing: $line"
+done < "$file"
---
+++ 24-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+####Installation of packages in shell script####
+USERID=$(id -u)
+R="\e[31m" #Red
+G="\e[32m" #Green
+Y="\e[33m" #Yellow
+N="\e[0m"  #No Color
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+LOG_FOLDER="/var/log/shellscript"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOG_FOLDER
+echo "script started at $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "ERROR: This script must be run as root $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+USAGE(){
+    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DESTINATION_DIR> <Days>[Optional, default 14 days] $N" | tee -a $LOG_FILE
+}
+if [ $# -le 2 ]; then
+    USAGE 
+fi
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R ERROR: $SOURCE_DIR directory does not exist $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+if [ ! -d DESTINATION_DIR ]; then
+    echo -e "$R ERROR: $DESTINATION_DIR directory does not exist $N" | tee -a $LOG_FILE
+    exit 1
+fi
