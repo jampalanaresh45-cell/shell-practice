4dd9dfed6b8bf96b938f648b9d55d65b03bcbc34
Naresh Jampala <jampalanaresh45@gmail.com>
2026-04-16 22:09:51 +0530

    shell-roboshop-common

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-
-while IFS= read -r line; do
-    echo "Processing: $line"
+DISK_THRESHOLD=2 ## In project we keepp it as 75 ##
+while IFS= read -r line; 
+do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -gt $DISK_THRESHOLD ]; then
+        echo "Disk usage for partition $PARTITION is above threshold: $USAGE%"
+    fi
 done <<< "$DISK_USAGE"
